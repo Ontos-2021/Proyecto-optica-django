4fb43d998a08c6a986b00493347f0026486fac86
VaneManzanelli <vanemanza1@gmail.com>
2021-08-29 21:47:03 -0300

    agregué formulario para Pacientes

--- secretaria/urls.py
+++ secretaria/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('nuevo_turno/', views.nuevoTurno, name='nuevo_turno'),
     path('modificar_turno/<str:pk>', views.modificarTurno, name='modificar_turno'),
     path('borrar_turno/<str:pk>', views.borrarTurno, name='borrar_turno'),
+    path('ver_paciente/<str:pk>', views.verPaciente, name='ver_paciente'),
+    path('nuevo_paciente/', views.nuevoPaciente, name='nuevo_paciente'),
+    path('borrar_paciente/<str:pk>', views.borrarPaciente, name='borrar_paciente'),
 ]
--- secretaria/views.py
+++ secretaria/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render, redirect
 from .models import Turno
-from .forms import TurnoForm
+from .forms import TurnoForm, PacienteForm
+from pacientes.models import Paciente
+
+
+def verPaciente(request, pk):
+    paciente = Paciente.objects.get(id=pk)
+    form = PacienteForm(instance=paciente)
+    if request.method == 'POST':
+        form = PacienteForm(request.POST, instance=paciente)
+        if form.is_valid():
+            form.save()
+        return redirect('/listado_turnos/')
+    context = {'form': form}
+    return render(request, 'secretaria/ver_paciente.html', context )  
+
+def nuevoPaciente(request):
+    form = PacienteForm
+    if request.method == 'POST':
+        form = PacienteForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return redirect('listado_turnos')
+       
+    context={'form': form}
+    return render(request, 'secretaria/nuevo_paciente.html', context)
+
+
+def borrarPaciente(request, pk):
+    paciente = Paciente.objects.get(id=pk)
+    if request.method == "POST":
+        paciente.delete() 
+        return redirect('listado_turnos')
+    context = {'paciente': paciente}
+    return render(request, 'secretaria/borrar_paciente.html', context)    
 
 
 def turnos(request):
-    
+    pacientes = Paciente.objects.all()
     turnos = Turno.objects.all()
         
-    context = {'turnos': turnos}
+    context = {'turnos': turnos, 'pacientes': pacientes}
     return render(request, 'secretaria/homeTurnos.html', context)
 
 def nuevoTurno(request):
